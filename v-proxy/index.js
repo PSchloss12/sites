ac552b97411324b13fcbcbf9a30ca7b4286c19a0
Patrick Schlosser <pschloss@nd.edu>
2024-07-19 16:09:45 -0400

    budle data into one endpoint

--- v-proxy/index.js
+++ v-proxy/index.js
@@ -23,6 +23,10 @@ const tradleUrl = "https://tradle.net/data.csv";
 // };
 // const reader = new FileReader();
 
+app.use((req, res, next) => {
+    console.log(`Received ${req.method} request for ${req.url}`);
+    next();
+});
 // Define a route that will act as a proxy
 app.get('/wordle/today', async (req, res) => {
     try {
@@ -82,6 +86,54 @@ app.get('/tradle', async (req, res) => {
 
 app.get('/test', (req,res) => {
     res.json({message: "Test endpoint hit"});
+})
+app.get('/', (req,res) => {
+    res.json({message: "Home endpoint hit"});
+})
+app.get('/api', async (req,res) => {
+    try {
+        const response0 = await axios.get(todayWordleUrl, {
+            headers: {
+                "Accept": "*/*",
+                "Accept-Encoding": "gzip, deflate, br, zstd",
+                "Accept-Language": "en-US,en;q=0.9",
+                "Content-Type": "application/x-www-form-urlencoded",
+                "Host": "www.nytimes.com",
+                "Referer": "https://www.nytimes.com/games/wordle/index.html",
+                "Sec-Fetch-Dest": "empty",
+                "Sec-Fetch-Mode": "cors",
+                "Sec-Fetch-Site": "same-origin",
+                }
+        });
+        const response1 = await axios.get(tomorrowWordleUrl, {
+            headers: {
+                "Accept": "*/*",
+                "Accept-Encoding": "gzip, deflate, br, zstd",
+                "Accept-Language": "en-US,en;q=0.9",
+                "Content-Type": "application/x-www-form-urlencoded",
+                "Host": "www.nytimes.com",
+                "Referer": "https://www.nytimes.com/games/wordle/index.html",
+                "Sec-Fetch-Dest": "empty",
+                "Sec-Fetch-Mode": "cors",
+                "Sec-Fetch-Site": "same-origin",
+                }
+        });
+        const response2 = await axios.get(tradleUrl, {
+            headers: {
+                "Referer": "https://tradle.net/"
+                }
+        });
+        let index = response2.data.search(dateToday);
+        res.json([
+            response0.data['solution'],
+            response1.data['solution'],
+            response2.data.slice(index+11, index+13),
+            response2.data.slice(index+26, index+28),
+        ]);
+    } catch(error) {
+        console.error('Error fetching data:',error);
+        res.status(500).send('Internal Server Error');
+    }
 })
 
 // Start the server
